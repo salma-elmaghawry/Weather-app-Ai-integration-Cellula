946e76e7c826960397f509f29125e98af7682c31
Salma Elmaghawry <salmaelmaghawry91@gmail.com>
2025-02-23 12:32:43 +0200

    get location usign geolocator

--- lib/features/Auth/presentation/screens/login_screen.dart
+++ lib/features/Auth/presentation/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:weather_app/core/helper/constant.dart';
 import 'package:weather_app/core/helper/extenstions.dart';
 import 'package:weather_app/core/routing/routes.dart';
@@ -7,8 +8,6 @@ import 'package:weather_app/core/widgets/custom_background.dart';
 import 'package:weather_app/core/widgets/custom_evaluated_button.dart';
 import 'package:weather_app/core/widgets/custom_textformfield.dart';
 import 'package:weather_app/core/widgets/line_with_action.dart';
-import 'package:weather_app/features/Auth/presentation/screens/register_screen.dart';
-import 'package:weather_app/features/home/presentation/screens/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   LoginScreen({super.key});
@@ -21,91 +20,135 @@ class _LoginScreenState extends State<LoginScreen> {
   final formKey = GlobalKey<FormState>();
 
   final TextEditingController email = TextEditingController();
-
   final TextEditingController password = TextEditingController();
 
   bool obscurePassword = true;
+
+  Future<Position> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+    
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomBackground(
-          child: Form(
-        key: formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: ListView(
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              SvgPicture.asset(signup, height: 200, width: 200),
-              const SizedBox(
-                height: 20,
-              ),
-              const Center(
-                child: Text(
-                  "Login",
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
+        child: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: ListView(
+              children: [
+                const SizedBox(
+                  height: 50,
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              CustomTextformfield(
-                controller: email,
-                label: "Email",
-                suffixIcon: const Icon(
-                  Icons.person,
-                  color: Colors.white,
+                SvgPicture.asset(signup, height: 200, width: 200),
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              CustomTextformfield(
-                controller: password,
-                label: "Password",
-                obscureText: obscurePassword,
-                suffixIcon: IconButton(
-                  color: Colors.white,
-                  icon: Icon(obscurePassword
-                      ? Icons.visibility_off
-                      : Icons.visibility),
-                  onPressed: () {
-                    setState(() {
-                      obscurePassword = !obscurePassword;
-                    });
+                const Center(
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomTextformfield(
+                  controller: email,
+                  label: "Email",
+                  suffixIcon: const Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomTextformfield(
+                  controller: password,
+                  label: "Password",
+                  obscureText: obscurePassword,
+                  suffixIcon: IconButton(
+                    color: Colors.white,
+                    icon: Icon(obscurePassword
+                        ? Icons.visibility_off
+                        : Icons.visibility),
+                    onPressed: () {
+                      setState(() {
+                        obscurePassword = !obscurePassword;
+                      });
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomEvaluatedButton(
+                  title: "Login",
+                  onTap: () async {
+                    if (formKey.currentState!.validate()) {
+                      try {
+                        Position position = await _getCurrentLocation();
+                        final searchValue =
+                            '${position.latitude},${position.longitude}';
+                        context.pushNamedAndRemoveUntil(
+                          Routes.homeScreen,
+                          arguments: {'searchValue': searchValue},
+                          routePredicate: (route) => false,
+                        );
+                      } catch (e) {
+                        print(e);
+                        // Handle the error appropriately, e.g., show a dialog or a snackbar
+                      }
+                    }
                   },
                 ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              CustomEvaluatedButton(
-                title: "Login",
-                onTap: () {
-                  context.pushNamedAndRemoveUntil(Routes.searchScreen,
-                      routePredicate: (route) => false);
-                },
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              LineWithAction(
-                title: "Don't Have Account ?",
-                actionName: " Register",
-                onTap: () {
-                  context.pushNamed(Routes.registerScreen);
-                },
-              ),
-            ],
+                const SizedBox(
+                  height: 20,
+                ),
+                LineWithAction(
+                  title: "Don't Have Account ?",
+                  actionName: " Register",
+                  onTap: () {
+                    context.pushNamed(Routes.registerScreen);
+                  },
+                ),
+              ],
+            ),
           ),
         ),
-      )),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:weather_app/core/routing/app_router.dart';
 
 import 'package:weather_app/weather_app.dart';
 
 void main() {
+   WidgetsFlutterBinding.ensureInitialized();
+  GeolocatorPlatform.instance;
   runApp( WeatherApp(appRouter:AppRouter() ));
 }
