fd9dcd541ba26cff05a8e3853ca7169de0c929d3
Salma Elmaghawry <salmaelmaghawry91@gmail.com>
2025-03-03 02:32:57 +0200

    adding firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather_app/core/routing/app_router.dart';
+import 'package:weather_app/firebase_options.dart';
 
 import 'package:weather_app/weather_app.dart';
 
-void main() {
+void main() async {
    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   GeolocatorPlatform.instance;
   runApp( WeatherApp(appRouter:AppRouter() ));
 }
